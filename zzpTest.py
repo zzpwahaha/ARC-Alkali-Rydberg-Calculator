8f15f5f0c5247c5d2254efdfd01894fcd3e5d586
Zhenpu Zhang <zhzh2597@colorado.edu>
2021-04-20 00:36:43 -0600

    add pycharm env

--- zzpTest.py
+++ zzpTest.py
@@ -12,3 +12,4 @@ LeRoy70Rb
 calculation = StarkMapResonances(atom,[70,1,1.5,1.5],atom,[70,1,1.5,1.5])
 
 calculation.findResonances(60,80,20,np.linspace(0,100,100),Bz=0,progressOutput=True)
+calculation.showPlot()
